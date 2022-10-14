69e20797b19523473872713e1b4797ed59228320
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-15 00:54:23 +0530

    Initial commit

---
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.msharibahmed.shaadidotcomassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
