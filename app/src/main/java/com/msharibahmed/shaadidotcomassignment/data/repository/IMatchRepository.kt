1056f8a97ba3563c936d2816f7bc69901859c591
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-15 20:48:25 +0530

    :memo: ADD: Repository methods

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/IMatchRepository.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/IMatchRepository.kt
@@ -1,4 +1,17 @@
 package com.msharibahmed.shaadidotcomassignment.data.repository
 
+import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
 interface IMatchRepository {
+    fun getMatchProfilesFromDb(): Flow<List<MatchProfile>> //local data
+
+    suspend fun getMatchesFromRemote(): Response<MatchResponseModel> //remote response data
+
+    suspend fun insertMatchProfiles(matches: List<MatchProfile>) //insert data to local db
+
+    suspend fun changeMatchStatus(matchProfile: MatchProfile) //update match profile status
+
 }
--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepositoryImpl.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepositoryImpl.kt
@@ -1,4 +1,28 @@
 package com.msharibahmed.shaadidotcomassignment.data.repository
 
-class MatchRepositoryImpl {
+import com.msharibahmed.shaadidotcomassignment.data.local.dao.MatchDao
+import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.data.remote.api.IApiService
+import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+import javax.inject.Inject
+
+class MatchRepositoryImpl @Inject constructor(
+    private val apiService: IApiService,
+    private val matchDao: MatchDao
+) : IMatchRepository {
+    override fun getMatchProfilesFromDb(): Flow<List<MatchProfile>> = matchDao.getAll()
+
+    override suspend fun getMatchesFromRemote(): Response<MatchResponseModel> =
+        apiService.getAllMatches()
+
+
+    override suspend fun insertMatchProfiles(matches: List<MatchProfile>) =
+        matchDao.insertAll(matches)
+
+
+    override suspend fun changeMatchStatus(matchProfile: MatchProfile) =
+        matchDao.update(uuid = matchProfile.uuid, status = matchProfile.status)
+
 }
