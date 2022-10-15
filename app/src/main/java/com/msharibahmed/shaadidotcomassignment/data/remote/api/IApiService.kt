f2113ed8eea8406c2fd8502f50e880a79eb9ae34
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-15 20:39:55 +0530

    ADD: api service methods

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/remote/api/IApiService.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/remote/api/IApiService.kt
@@ -1,4 +1,10 @@
 package com.msharibahmed.shaadidotcomassignment.data.remote.api
 
+import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface IApiService {
+    @GET("api/?results=10")
+    suspend fun getAllMatches(): Response<MatchResponseModel>
 }
