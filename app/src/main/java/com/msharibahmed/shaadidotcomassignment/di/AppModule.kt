49a0df82efcbde43d96fed6918d055df674774e3
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-15 21:15:51 +0530

    :memo: ADD: glide instance in app module

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/di/AppModule.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/di/AppModule.kt
@@ -1,4 +1,27 @@
 package com.msharibahmed.shaadidotcomassignment.di
 
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.msharibahmed.shaadidotcomassignment.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_account_circle)
+            .error(R.drawable.ic_account_circle)
+    )
 }
