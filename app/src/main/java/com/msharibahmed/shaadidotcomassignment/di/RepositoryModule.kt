dd611dd3851b02145405446a6a9d3b6d3083f085
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-15 20:55:51 +0530

    :memo: ADD: Repository module

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/di/RepositoryModule.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/di/RepositoryModule.kt
@@ -1,4 +1,14 @@
 package com.msharibahmed.shaadidotcomassignment.di
 
-object RepositoryModule {
+import com.msharibahmed.shaadidotcomassignment.data.repository.MatchRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+    @Binds
+    fun MatchRepository(matchRepository: MatchRepository): MatchRepository
 }
