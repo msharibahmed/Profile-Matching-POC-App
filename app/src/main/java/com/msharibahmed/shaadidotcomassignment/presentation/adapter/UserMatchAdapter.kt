542c68c93b8a19907bbe7acf7967595d1999406b
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-16 19:08:38 +0530

    ADD: adapter + logic + listeners

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/presentation/adapter/UserMatchAdapter.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/presentation/adapter/UserMatchAdapter.kt
@@ -1,4 +1,98 @@
 package com.msharibahmed.shaadidotcomassignment.presentation.adapter
 
-class UserMatchAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.msharibahmed.shaadidotcomassignment.R
+import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.databinding.MatchProfileCardBinding
+import com.msharibahmed.shaadidotcomassignment.utils.Status
+import javax.inject.Inject
+
+
+class UserMatchAdapter @Inject constructor(private val glide: RequestManager) :
+    ListAdapter<MatchProfile, UserMatchAdapter.MyViewHolder>(userMatchAdapterDiffer) {
+
+    companion object {
+        val userMatchAdapterDiffer = object : DiffUtil.ItemCallback<MatchProfile>() {
+            override fun areItemsTheSame(oldItem: MatchProfile, newItem: MatchProfile): Boolean =
+                oldItem.uuid == newItem.uuid
+
+            override fun areContentsTheSame(oldItem: MatchProfile, newItem: MatchProfile): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            MatchProfileCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    private var changeProfileStatusListener: ((MatchProfile, Status) -> Unit)? = null
+
+    fun assignChangeProfileStatusListener(listener: (MatchProfile, Status) -> Unit) {
+        changeProfileStatusListener = listener
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val profile = getItem(position)
+        holder.bind(profile)
+    }
+
+    inner class MyViewHolder(private val binding: MatchProfileCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(profile: MatchProfile) {
+            val context = binding.root.context
+
+            binding.apply {
+                glide.load(profile.imageUrl).into(ivProfile)
+                "${profile.firstName} ${profile.lastName}".also { tvName.text = it }
+                tvLocation.text = profile.country
+                "${profile.age} years old".also { tvAge.text = it }
+
+                when (profile.status) {
+                    Status.PENDING -> {
+                        groupSelection.visibility = View.VISIBLE
+                        tvMessage.visibility = View.GONE
+                    }
+                    Status.ACCEPTED -> {
+                        groupSelection.visibility = View.GONE
+                        tvMessage.visibility = View.VISIBLE
+
+                        tvMessage.text = context.getString(R.string.match_accepted_message)
+                    }
+                    Status.REJECTED -> {
+                        groupSelection.visibility = View.GONE
+                        tvMessage.visibility = View.VISIBLE
+
+                        tvMessage.text = context.getString(R.string.match_rejected_message)
+                    }
+                }
+
+                btnAccept.setOnClickListener {
+                    changeProfileStatusListener?.invoke(profile, Status.ACCEPTED)
+                    tvMessage.text = context.getString(R.string.match_accepted_message)
+                    notifyItemChanged(position)
+                }
+
+                btnDecline.setOnClickListener {
+                    changeProfileStatusListener?.invoke(profile, Status.REJECTED)
+                    tvMessage.text = context.getString(R.string.match_rejected_message)
+                    notifyItemChanged(position)
+                }
+
+
+            }
+
+
+        }
+
+    }
+
 }
