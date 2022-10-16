bf291a51fa1e34774a65c0b78827c8052c22c188
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-16 19:07:41 +0530

    UPDATE: modification for storing and getting data from db

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepository.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepository.kt
@@ -2,13 +2,15 @@ package com.msharibahmed.shaadidotcomassignment.data.repository
 
 import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
 import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+import com.msharibahmed.shaadidotcomassignment.data.remote.model.Result
+import com.msharibahmed.shaadidotcomassignment.utils.events.ResponseState
 import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 
 interface MatchRepository {
     fun getMatchProfilesFromDb(): Flow<List<MatchProfile>> //local data
 
-    suspend fun getMatchesFromRemote(): Response<MatchResponseModel> //remote response data
+    suspend fun getMatchesFromRemote(): Flow<ResponseState<List<MatchProfile>>> //remote response data
 
     suspend fun insertMatchProfiles(matches: List<MatchProfile>) //insert data to local db
 
--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepositoryImpl.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/repository/MatchRepositoryImpl.kt
@@ -2,20 +2,38 @@ package com.msharibahmed.shaadidotcomassignment.data.repository
 
 import com.msharibahmed.shaadidotcomassignment.data.local.dao.MatchDao
 import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.data.local.toEntityMapper.ToMatchProfileMapper.matchRemoteResponseToEntityMapper
 import com.msharibahmed.shaadidotcomassignment.data.remote.api.IApiService
-import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+import com.msharibahmed.shaadidotcomassignment.utils.events.ResponseState
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Response
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MatchRepositoryImpl @Inject constructor(
     private val apiService: IApiService,
     private val matchDao: MatchDao
 ) : MatchRepository {
+
+    private val matchProfiles = getMatchProfilesFromDb()
+
     override fun getMatchProfilesFromDb(): Flow<List<MatchProfile>> = matchDao.getAll()
 
-    override suspend fun getMatchesFromRemote(): Response<MatchResponseModel> =
-        apiService.getAllMatches()
+    override suspend fun getMatchesFromRemote(): Flow<ResponseState<List<MatchProfile>>> =
+        flow {
+
+            emit(ResponseState.Loading)
+            try {
+                val matchResponse =
+                    apiService.getAllMatches().body()
+                //
+                matchDao.insertAll(matchRemoteResponseToEntityMapper(matchResponse))
+                //
+                emit(ResponseState.Success(matchProfiles.first()))
+            } catch (e: Exception) {
+                emit(ResponseState.Error(e, matchProfiles.first()))
+            }
+        }
 
 
     override suspend fun insertMatchProfiles(matches: List<MatchProfile>) =
@@ -24,5 +42,6 @@ class MatchRepositoryImpl @Inject constructor(
 
     override suspend fun changeMatchStatus(matchProfile: MatchProfile) =
         matchDao.update(uuid = matchProfile.uuid, status = matchProfile.status)
+
 
 }
