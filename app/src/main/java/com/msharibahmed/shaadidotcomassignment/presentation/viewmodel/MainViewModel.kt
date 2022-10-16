02120eb1696aec37b32ac240e3902adb01eb4a89
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-16 19:07:13 +0530

    ADD: view model business logic

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/presentation/viewmodel/MainViewModel.kt
@@ -1,4 +1,48 @@
 package com.msharibahmed.shaadidotcomassignment.presentation.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.*
+import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.data.repository.MatchRepository
+import com.msharibahmed.shaadidotcomassignment.utils.Status
+import com.msharibahmed.shaadidotcomassignment.utils.events.ResponseState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel
+@Inject
+constructor(
+    private val matchRepository: MatchRepository,
+    private val coroutineDispatcher: CoroutineDispatcher,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel(), LifecycleObserver {
+
+    private val _responseState: MutableLiveData<ResponseState<List<MatchProfile>>> =
+        MutableLiveData()
+
+    val responseState: LiveData<ResponseState<List<MatchProfile>>>
+        get() = _responseState
+
+    fun getMatchProfiles() {
+        viewModelScope.launch(coroutineDispatcher) {
+            matchRepository.getMatchesFromRemote()
+                .onEach {
+                    _responseState.value = it
+                }.launchIn(viewModelScope)
+
+        }
+    }
+
+    fun changeMatchProfileStatus(matchProfile: MatchProfile, status: Status) {
+        viewModelScope.launch(coroutineDispatcher) {
+            matchProfile.status = status
+            matchRepository.changeMatchStatus(matchProfile)
+        }
+    }
+
+
 }
