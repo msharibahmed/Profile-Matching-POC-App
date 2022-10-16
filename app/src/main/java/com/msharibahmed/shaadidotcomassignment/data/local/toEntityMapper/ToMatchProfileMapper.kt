5c9e4de138942a7cfedb9988561d77d236ece196
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-16 19:05:44 +0530

    ADD: ToEntityMapper

---
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/data/local/toEntityMapper/ToMatchProfileMapper.kt
@@ -0,0 +1,27 @@
+package com.msharibahmed.shaadidotcomassignment.data.local.toEntityMapper
+
+import com.msharibahmed.shaadidotcomassignment.data.local.entities.MatchProfile
+import com.msharibahmed.shaadidotcomassignment.data.remote.model.MatchResponseModel
+
+object ToMatchProfileMapper {
+    fun matchRemoteResponseToEntityMapper(matchNetworkResponse: MatchResponseModel?): List<MatchProfile> {
+
+        var matchProfiles: List<MatchProfile>? = null
+
+        matchNetworkResponse?.let { response ->
+            matchProfiles = response.results.map {
+                MatchProfile(
+                    uuid = it.login.uuid,
+                    firstName = it.name.first,
+                    lastName = it.name.last,
+                    imageUrl = it.picture.large,
+                    age = it.dob.age,
+                    email = it.email,
+                    country = it.location.country
+                )
+            }
+        }
+
+        return matchProfiles ?: mutableListOf()
+    }
+}
