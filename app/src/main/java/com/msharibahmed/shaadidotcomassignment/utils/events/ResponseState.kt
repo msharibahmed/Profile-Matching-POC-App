738ebdc757af74938611a101c7c36ff7327ba2ad
Sharib Ahmed <msharibahmed@gmail.com>
2022-10-16 19:06:29 +0530

    UPDATE: responseState for error state

--- app/src/main/java/com/msharibahmed/shaadidotcomassignment/utils/events/ResponseState.kt
+++ app/src/main/java/com/msharibahmed/shaadidotcomassignment/utils/events/ResponseState.kt
@@ -3,6 +3,6 @@ package com.msharibahmed.shaadidotcomassignment.utils.events
 sealed class ResponseState<out R> {
 
     data class Success<out T>(val data: T) : ResponseState<T>()
-    data class Error(val exception: Exception) : ResponseState<Nothing>()
+    data class Error<out T>(val exception: Exception, val data: T) : ResponseState<T>()
     object Loading : ResponseState  <Nothing>()
 }
